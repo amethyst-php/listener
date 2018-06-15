939d8a7e355595f742b4eac1a847a5d61c10dfc0
Railken <railken@protonmail.ch>
2018-06-15 12:53:55 +0000

    Apply fixes from StyleCI

--- database/migrations/2018_06_04_100001_create_listeners_table.php
+++ database/migrations/2018_06_04_100001_create_listeners_table.php
@@ -1,9 +1,9 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Schema;
 
 class CreateListenersTable extends Migration
 {
@@ -15,16 +15,16 @@ class CreateListenersTable extends Migration
     public function up()
     {
         Schema::create(Config::get('ore.listener.table'), function (Blueprint $table) {
-            $table->increments('id'); 
-            $table->string('name'); 
-            $table->string('condition')->default("1");
-            $table->text('description')->nullable(); 
-            $table->boolean('entities')->default(0); 
-            $table->text('event_class'); 
-            $table->integer('work_id')->unsigned()->nullable(); 
-            $table->foreign('work_id')->references('id')->on('ore_works'); 
+            $table->increments('id');
+            $table->string('name');
+            $table->string('condition')->default('1');
+            $table->text('description')->nullable();
+            $table->boolean('entities')->default(0);
+            $table->text('event_class');
+            $table->integer('work_id')->unsigned()->nullable();
+            $table->foreign('work_id')->references('id')->on('ore_works');
             $table->boolean('enabled')->default(true);
-            $table->timestamps(); 
+            $table->timestamps();
         });
     }
 
--- tests/ApiTest.php
+++ tests/ApiTest.php
@@ -2,8 +2,8 @@
 
 namespace Railken\LaraOre\Listener\Tests;
 
-use Railken\LaraOre\Support\Testing\ApiTestableTrait;
 use Illuminate\Support\Facades\Config;
+use Railken\LaraOre\Support\Testing\ApiTestableTrait;
 
 class ApiTest extends BaseTest
 {
