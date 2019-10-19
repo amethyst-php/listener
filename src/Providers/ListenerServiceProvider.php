ac8abc6571b0fd8fc314a4735b46bef8b578c8ee
Railken <railken@protonmail.ch>
2019-10-19 17:41:23 +0200

    Update ListenerServiceProvider.php

--- src/Providers/ListenerServiceProvider.php
+++ src/Providers/ListenerServiceProvider.php
@@ -57,8 +57,6 @@ class ListenerServiceProvider extends CommonServiceProvider
                         }
                     }
                 }
-
-                return false;
             });
         }
     }
